752ee052966b6b8201d724df294cc842364d6654
Anderson <@1001ander>
2025-07-23 20:22:30 -0400

    Crear el modelo CarritoCompras

---
+++ ProyectoFinalAp1/Models/CarritoCompra.cs
@@ -0,0 +1,19 @@
+﻿using ProyectoFinalAp1.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models
+{
+    public class CarritoCompra
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(IdUsuario))]
+        public string IdUsuario { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public List<CarritoDetalle> Detalles { get; set; } = new();
+        public ApplicationUser Usuario { get; set; }
+    }
+}
