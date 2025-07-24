cec159c8c4d64d1f5f768531085d0b50c925b5f5
Anderson <@1001ander>
2025-07-23 20:35:28 -0400

    Crear el modelo carrito compra

--- ProyectoFinalAp1/Models/CarritoCompra.cs
+++ ProyectoFinalAp1/Models/CarritoCompra.cs
@@ -2,18 +2,17 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ProyectoFinalAp1.Models
+namespace ProyectoFinalAp1.Models;
+
+public class CarritoCompra
 {
-    public class CarritoCompra
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        [ForeignKey(nameof(IdUsuario))]
-        public string IdUsuario { get; set; }
+    [ForeignKey(nameof(IdUsuario))]
+    public string IdUsuario { get; set; }
 
-        public DateTime FechaCreacion { get; set; } = DateTime.Now;
-        public List<CarritoDetalle> Detalles { get; set; } = new();
-        public ApplicationUser Usuario { get; set; }
-    }
+    public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    public List<CarritoDetalle> Detalles { get; set; } = new();
+    public ApplicationUser Usuario { get; set; }
 }
