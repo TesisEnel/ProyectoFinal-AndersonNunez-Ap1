088a3db713945dac76ce030a4fed806c5a4fcc9d
Anderson <@1001ander>
2025-07-23 20:34:17 -0400

    Crear el contexto.

--- ProyectoFinalAp1/Data/ApplicationDbContext.cs
+++ ProyectoFinalAp1/Data/ApplicationDbContext.cs
@@ -1,9 +1,25 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAp1.Models;
 
 namespace ProyectoFinalAp1.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+  
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aviones> Avions { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<CarritoCompra> CarritoCompras { get; set; }
+        public DbSet<CarritoDetalle> CarritoDetalles { get; set; }
+
+
+
     }
 }
