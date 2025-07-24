c9eaddcb28cceb4cdd90e2bf4be6de79eec7c538
Anderson <@1001ander>
2025-07-23 20:22:06 -0400

    Crear el modelo categorias

---
+++ ProyectoFinalAp1/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAp1.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int IdCategoria { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string? Nombre { get; set; }
+
+        public string? Descripcion { get; set; }
+    }
+}
