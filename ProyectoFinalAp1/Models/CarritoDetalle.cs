b7a736577ef1db6367474e58c9a30280f00dc8f5
Anderson <@1001ander>
2025-07-23 20:23:06 -0400

    Crear el modelo CarritoDetalle.

---
+++ ProyectoFinalAp1/Models/CarritoDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoFinalAp1.Models
+{
+    public class CarritoDetalle
+    {
+        public int Id { get; set; }
+        public int IdAvion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Total => Cantidad * Precio;
+
+        [ForeignKey(nameof(IdAvion))]
+        public Aviones? Avion { get; set; }
+    }
+}
